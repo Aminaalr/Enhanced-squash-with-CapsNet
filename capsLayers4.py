66053597c0895a9460fcf57e4c8b262dae325070
Amina_ Alrahawi <116610804+Aminaalr@users.noreply.github.com>
2024-05-11 12:37:02 +0300

    Add files via upload

---
+++ capsLayers4.py
@@ -0,0 +1,219 @@
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from tensorflow.keras import initializers, layers
+import numpy as np
+
+
+class Length(layers.Layer):
+    def call(self, inputs, **kwargs):
+        return tf.sqrt(tf.reduce_sum(tf.square(inputs), -1) + K.epsilon())
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[:-1]
+
+    def get_config(self):
+        config = super(Length, self).get_config()
+        return config
+
+
+class Mask(layers.Layer):
+    def call(self, inputs, **kwargs):
+        if type(inputs) is list:
+            assert len(inputs) == 2
+            inputs, mask = inputs
+        else:
+            x = tf.sqrt(tf.reduce_sum(tf.square(inputs), -1))
+            mask = tf.one_hot(indices=tf.argmax(x, 1), depth=x.shape[1])
+        masked = K.batch_flatten(inputs * tf.expand_dims(mask, -1))
+        return masked
+
+    def compute_output_shape(self, input_shape):
+        if type(input_shape[0]) is tuple:
+            return tuple([None, input_shape[0][1] * input_shape[0][2]])
+        else:
+            return tuple([None, input_shape[1] * input_shape[2]])
+
+    def get_config(self):
+        config = super(Mask, self).get_config()
+        return config
+
+
+import tensorflow as tf
+#def squash(vectors, axis=-1):
+    #s_squared_norm = tf.reduce_sum(tf.square(vectors), axis, keepdims=True)
+    #scale = s_squared_norm / (1 + s_squared_norm) / tf.sqrt(s_squared_norm + K.epsilon())
+    #squashed = scale * vectors
+    #squashed = tf.tanh(squashed)  # Squash using tanh activation
+    #return squashed
+#def squash(vectors, axis=-1, alpha=0.5, beta=1.0):
+    #s_squared_norm = tf.reduce_sum(tf.square(vectors), axis, keepdims=True)
+    #scale = tf.sigmoid(beta * (s_squared_norm ** alpha)) 
+    #return scale * vectors
+#def squash(vectors, axis=-1, sharpness=0.5):
+ #   s_squared_norm = tf.reduce_sum(tf.square(vectors), axis, keepdims=True)
+  #  scale = tf.sigmoid(sharpness * (s_squared_norm / (1 + s_squared_norm)))
+   # return scale * vectors
+#def squash(vectors, axis=-1, alpha=0.5):
+ #   s_squared_norm = tf.reduce_sum(tf.square(vectors), axis, keepdims=True)
+  #  scale = (s_squared_norm ** alpha) / (1 + s_squared_norm) 
+   # return scale * vectors
+#def squash(vectors, axis=-1):
+    #s_squared_norm = tf.reduce_sum(tf.square(vectors), axis, keepdims=True)
+    #scale = s_squared_norm / (1 + s_squared_norm) / tf.sqrt(s_squared_norm + K.epsilon())
+    #return scale * vectors
+#def squash(vectors, axis=-1):
+ #   s_squared_norm = tf.reduce_sum(tf.square(vectors), axis, keepdims=True)
+  #  scale = 1 / (1 + s_squared_norm)/4
+   # return scale * vectors
+#def squash(vectors, axis=-1):
+  
+  #with tf.name_scope('squash'):
+    #squared_norm = tf.reduce_sum(tf.square(vectors), axis=axis, keepdims=True)
+    #norm = tf.sqrt(squared_norm + K.epsilon())
+    #one_minus_exp_norm = 1 - tf.exp(norm)
+    #scale = (1 / one_minus_exp_norm) * vectors / norm
+    #return scale
+''' 
+def squash(vectors, axis=-1, alpha_param=None, epsilon=tf.keras.backend.epsilon()):
+    s_squared_norm = tf.reduce_sum(tf.square(vectors), axis=axis, keepdims=True)
+    if alpha_param is None:
+       alpha_initializer = tf.ones_like(s_squared_norm)
+       alpha = layers.Dense(1, use_bias=False, kernel_initializer=initializers.Constant(alpha_initializer))(s_squared_norm)
+    else:
+       alpha = alpha_param
+    scale = alpha / (1.0 + alpha * s_squared_norm)
+    return scale * vectors
+''' 
+'''
+def squash(vectors, axis=-1):
+    s_squared_norm = tf.reduce_sum(tf.square(vectors), axis, keepdims=True) + K.epsilon()
+    squashing_factor = s_squared_norm / (0.5 + s_squared_norm)
+    squash = squashing_factor * vectors / tf.sqrt(s_squared_norm)
+    # Non-linear activation (tanh, sigmoid)
+    squashed = tf.tanh(squash)
+    return squashed
+'''
+''''
+#s200
+def squash(vectors, axis=-1):
+  s_squared_norm = tf.reduce_sum(tf.square(vectors), axis, keepdims=True)
+  scale = s_squared_norm / (1 + s_squared_norm) / tf.sqrt(s_squared_norm + K.epsilon())
+  squashed_vectors = scale * vectors
+  # Additional Analysis
+  mean_magnitude = tf.reduce_mean(tf.sqrt(s_squared_norm), axis=axis)
+  return squashed_vectors, mean_magnitude
+  '''
+
+#Litrature1 Squash by //Afriyie, Y., A. Weyori, B., & A. Opoku, A. (2022). Classification of blood cells using optimized Capsule networks. Neural Processing Letters, 54(6), 4809-4828///
+'''
+def squash(vectors, axis=-1):
+    s_squared_norm = tf.reduce_sum(tf.square(vectors), axis, keepdims=True)
+    scale = 1 / (1 + s_squared_norm)/2
+    return scale *  vectors
+'''
+'''
+#Litrature1 2 Squash User f(x) = (1 - 1 / exp(||x||)) * (x / ||x||) 
+import tensorflow as tf
+
+def squash(x):
+    """
+    Squashing function implementation using TensorFlow operations.
+    
+    Arguments:
+    x -- Input tensor
+    
+    Returns:
+    squashed -- Squashed output tensor
+    """
+    # Calculate the squared norm of the input tensor
+    squared_norm = tf.reduce_sum(tf.square(x), axis=-1, keepdims=True)
+    
+    # Compute the square root of the squared norm
+    norm = tf.sqrt(squared_norm)
+    
+    # Compute the exponential term
+    exp_term = tf.exp(-norm)
+    
+    # Compute the denominator term
+    denom = 1 - (1 / (1 + exp_term))
+    
+    # Compute the squashed output
+    squashed = (x / norm) * denom
+    
+    return squashed
+'''
+
+
+
+
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+def squash(vectors, axis=-1):
+    # Non-linear activation function (squashing)
+    s_squared_norm = tf.reduce_sum(tf.square(vectors), axis, keepdims=True)
+    scale = 0.5 * s_squared_norm / (1+ 0.5* s_squared_norm) /  tf.sqrt(s_squared_norm + K.epsilon())
+    # Layer normalization
+    epsilon = 1e-6
+    mean, variance = tf.nn.moments(vectors, axes=-1, keepdims=True)
+    vectors = (vectors - mean) / tf.sqrt(variance + epsilon)
+    
+    return scale * vectors
+
+
+
+ 
+class CapsuleLayer(layers.Layer):
+    def __init__(self, num_capsule, dim_capsule, routings=3,
+                 kernel_initializer='glorot_uniform',
+                 **kwargs):
+        super(CapsuleLayer, self).__init__(**kwargs)
+        self.num_capsule = num_capsule
+        self.dim_capsule = dim_capsule
+        self.routings = routings
+        self.kernel_initializer = initializers.get(kernel_initializer)
+
+    def build(self, input_shape):
+        assert len(input_shape) >= 3, "The input Tensor should have shape=[None, input_num_capsule, input_dim_capsule]"
+        self.input_num_capsule = input_shape[1]
+        self.input_dim_capsule = input_shape[2]
+        
+        self.W = self.add_weight(shape=[self.num_capsule, self.input_num_capsule,
+                                        self.dim_capsule, self.input_dim_capsule],
+                                 initializer=self.kernel_initializer,
+                                 name='W')
+
+        self.built = True
+
+    def call(self, inputs, training=None):        
+        inputs_expand = tf.expand_dims(tf.expand_dims(inputs, 1), -1)
+        inputs_tiled = tf.tile(inputs_expand, [1, self.num_capsule, 1, 1, 1])
+        inputs_hat = tf.squeeze(tf.map_fn(lambda x: tf.matmul(self.W, x), elems=inputs_tiled))
+        b = tf.zeros(shape=[inputs.shape[0], self.num_capsule, 1, self.input_num_capsule])
+        assert self.routings > 0, 'The routings should be > 0.'
+        for i in range(self.routings):
+            c = tf.nn.softmax(b, axis=1)
+            outputs = squash(tf.matmul(c, inputs_hat))  
+            if i < self.routings - 1:
+                b += tf.matmul(outputs, inputs_hat, transpose_b=True)      
+
+        return tf.squeeze(outputs)
+
+    def compute_output_shape(self, input_shape):
+        return tuple([None, self.num_capsule, self.dim_capsule])
+
+    def get_config(self):
+        config = {
+            'num_capsule': self.num_capsule,
+            'dim_capsule': self.dim_capsule,
+            'routings': self.routings
+        }
+        base_config = super(CapsuleLayer, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+def PrimaryCap(inputs, dim_capsule, n_channels, kernel_size, strides, padding):
+    output = layers.Conv2D(filters=dim_capsule*n_channels, kernel_size=kernel_size, strides=strides, padding=padding,
+                           name='primarycap_conv2d')(inputs)
+    outputs = layers.Reshape(target_shape=[-1, dim_capsule], name='primarycap_reshape')(output)
+    return layers.Lambda(squash, name='primarycap_squash')(outputs)
